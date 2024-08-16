04cf9ae5015d8a5b058384e0457f5e14d7aae178
Sonil207 <113001910+Sonil207@users.noreply.github.com>
2024-08-17 02:04:29 +0530

    Add files via upload

---
+++ service-registry/src/test/java/com/sonil/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonil.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
